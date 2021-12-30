99022c20792c8c56c18f061ddd60618ab273b7f2
srshashibhushan <30664313+ReeSSB@users.noreply.github.com>
2021-12-30 18:29:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,337 @@
+"use strict";
+
+const resume = {
+        name: "Shashi Bhushan",
+        city: "Noida",
+        experience: {
+                firstCompany: {
+                        companyName: "TATA AIG GIC.",
+                        location: "Noida",
+                        position: "Relationship Manager",
+                        joiningDate: `Aug - 2019`,
+                        lastWorkingDate: `Feb - 2021`,
+                        description: `
+                                Channel: Bancassurance
+                                Vertical : IndusInd Branch Banking & IndusInd CFD.
+                                Region : Noida, Greater Noida & Ghaziabad for Branch Banking.
+                                Delhi, Noida & Ghaziabad for CFD.
+
+                                ● Managing relationships with all branches falls in the region,
+                                to direct business by providing the best possible solution for
+                                customer need related Insurance.
+                                ● Providing after-sales service by guiding clients through the
+                                process related to claim/endorsement/discrepancies.
+                                ● Involved in continuous training of channel partner (new &
+                                on-going salespeople) & in their meetings related insurance,
+                                whether it involves individual customers or corporate clients.
+                                ● Involved in continuous generation of leads & follow-ups with
+                                prospective clients.
+                                ● Focused on business growth & retention.
+                                ● Responsible & accountable for decision - making with partners to achieve the targets.`,
+                },
+                secondCompany: {
+                        companyName: "BAJAJ ALLIANZ GIC.",
+                        location: "Noida",
+                        position: "Sales Manager",
+                        joiningDate: `Mar - 2021`,
+                        lastWorkingDate: `Jul - 2021`,
+                        description: `   
+                                Channel: Bancassurance
+                                Vertical : IndusInd Branch Banking & IndusInd CFD.
+                                Region : Noida, Greater Noida & Ghaziabad for Branch Banking.
+                                Delhi, Noida & Ghaziabad for CFD.
+
+                                ● Managing relationships with all branches falls in the region,
+                                to direct business by providing the best possible solution for
+                                customer need related Insurance.
+                                ● Providing after-sales service by guiding clients through the
+                                process related to claim/endorsement/discrepancies.
+                                ● Involved in continuous training of channel partner (new &
+                                on-going salespeople) & in their meetings related insurance,
+                                whether it involves individual customers or corporate clients.
+                                ● Involved in continuous generation of leads & follow-ups with
+                                prospective clients.
+                                ● Focused on business growth & retention.
+                                ● Responsible & accountable for decision - making with partners to achieve the targets.`,
+                },
+        },
+        education: {
+                school: {
+                        intermediate: {
+                                passYear: 2012,
+                                subject: `Science`,
+                                schoolName: "TATA DAV Public School",
+                                board: "CBSE",
+                        },
+                        matriculation: {
+                                passYear: 2009,
+                                subject: `Science`,
+                                schoolName: "TATA DAV Public School",
+                                board: "CBSE",
+                        },
+                },
+                underGraduate: {
+                        passYear: 2016,
+                        subject: "Bachelor of Business Administration",
+                        subjectCode: "BBA (I.T)",
+                        specialization: "Information Technology",
+                        universityName: "Lovely Professional University",
+                        startDate: 2012,
+                        startDateMonth: "Aug",
+                        endDate: 2016,
+                        endDateMonth: "Feb",
+                        city: "Phagwara",
+                        state: "Punjab",
+                        country: "India",
+                },
+                postGraduate: {
+                        passYear: 2020,
+                        subject: "Post Graduate Diploma in General Insurance",
+                        subjectCode: "PGDGI",
+                        universityName: "Manipal Academy of Higher Education",
+                        startDate: 2019,
+                        startDateMonth: "Apr",
+                        endDate: 2020,
+                        endDateMonth: "May",
+                        city: "Bangalore",
+                        state: "karnataka",
+                        country: "India",
+                },
+        },
+        contact: {
+                contactNo: 6203387681,
+                emailAddress: "shashi.as05@gmail.com",
+                address: `8 B 1ST FLOOR TYPE 1 BLOCK
+                11 SHREE KRISHNA APARTMENT
+                SEC 117 NOIDA GAUTAM BUDDHA
+                NAGAR UTTAR PRADESH 201307 `,
+                linkedInId: `www.linkedin.com/in/srshashibhushannbhardwaj`,
+        },
+        skills: {
+                softSkill: [
+                        "Team Player",
+                        "Dedicated",
+                        "Trustworthy",
+                        "Analytical",
+                        "Result Oriented",
+                        " Good at Communication",
+                ],
+                ITSkill: [
+                        "HTML",
+                        "CSS",
+                        "Javascript",
+                        "React",
+                        "MonoDB",
+                        "NodeJs",
+                        "MS Office",
+                        "Excel",
+                        "MS Power Point",
+                ],
+                others: [],
+        },
+        language: {
+                hindi: {
+                        name: "Hindi",
+                        speak: true,
+                        write: true,
+                        fluency: "Mother tongue",
+                },
+                english: {
+                        name: "English",
+                        speak: true,
+                        write: true,
+                        fluency: "Fluent",
+                },
+        },
+};
+
+const resume2 = {
+        Name: "Shashi Bhushan",
+        City: "Noida",
+        Experience1: `companyName: TATA AIG GIC
+                                location: Noida
+                                position: Relationship Manager
+                                joiningDate: Aug - 2019
+                                lastWorkingDate: Feb - 2021
+
+                                Description:
+                                Channel: Bancassurance
+                                Vertical : IndusInd Branch Banking & IndusInd CFD.
+                                Region : Noida, Greater Noida & Ghaziabad for Branch Banking.
+                                Delhi, Noida & Ghaziabad for CFD.
+
+                                ● Managing relationships with all branches falls in the region,
+                                to direct business by providing the best possible solution for
+                                customer need related Insurance.
+                                ● Providing after-sales service by guiding clients through the
+                                process related to claim/endorsement/discrepancies.
+                                ● Involved in continuous training of channel partner (new &
+                                on-going salespeople) & in their meetings related insurance,
+                                whether it involves individual customers or corporate clients.
+                                ● Involved in continuous generation of leads & follow-ups with
+                                prospective clients.
+                                ● Focused on business growth & retention.
+                                ● Responsible & accountable for decision - making with partners to achieve the targets.`,
+
+        Experience2: `companyName: BAJAJ ALLIANZ GIC.
+                                location: Noida
+                                position: Sales Manager
+                                joiningDate: Mar - 2021
+                                lastWorkingDate: Jul - 2021
+
+                                Description: 
+                                Channel: Bancassurance
+                                Vertical : IndusInd Branch Banking & IndusInd CFD.
+                                Region : Noida, Greater Noida & Ghaziabad for Branch Banking.
+                                Delhi, Noida & Ghaziabad for CFD.
+
+                                ● Managing relationships with all branches falls in the region,
+                                to direct business by providing the best possible solution for
+                                customer need related Insurance.
+                                ● Providing after-sales service by guiding clients through the
+                                process related to claim/endorsement/discrepancies.
+                                ● Involved in continuous training of channel partner (new &
+                                on-going salespeople) & in their meetings related insurance,
+                                whether it involves individual customers or corporate clients.
+                                ● Involved in continuous generation of leads & follow-ups with
+                                prospective clients.
+                                ● Focused on business growth & retention.
+                                ● Responsible & accountable for decision - making with partners to achieve the targets.`,
+        Intermediate: `Year: 2012
+                                Subject: Science
+                                TATA DAV Public School
+                                Board: CBSE`,
+
+        Matriculation: `Year: 2009
+                                subject: Science
+                                schoolName: TATA DAV Public School
+                                Board: CBSE`,
+        Under_Graduate: `Year: 2016
+                                subject: "Bachelor of Business Administration",
+                                Lovely Professional University
+                                2012 to 2016
+                                India`,
+
+        Post_Graduate: `Year: 2020,
+                                subject: Post Graduate Diploma in General Insurance
+                                University Name: "Manipal Academy of Higher Education",
+                                2019-2020
+                                India`,
+        Contact_No: `6203387681
+                                shashi.as05@gmail.com
+                                Address: 8 B 1ST FLOOR TYPE 1 BLOCK
+                                         11 SHREE KRISHNA APARTMENT
+                                         SEC 117 NOIDA GAUTAM BUDDHA
+                                         NAGAR UTTAR PRADESH 201307 
+                                linkedInId: www.linkedin.com/in/srshashibhushannbhardwaj`,
+        Soft_Skill: `Team Player
+                                Dedicated
+                                Trustworthy
+                                Analytical
+                                Result Oriented
+                                Good at Communication`,
+
+        IT_Skill: `HTML
+                                CSS
+                                Javascript
+                                React
+                                MonoDB
+                                NodeJs
+                                MS Office
+                                Excel
+                                MS Power Point`,
+        Language: `Hindi, English`,
+};
+
+
+
+const resumeLength = Object.keys(resume).length;
+
+// console.log(Object.keys(resume));
+console.log(resume);
+
+
+//Using FOR LOOP FUNCTION
+console.log(`*************Using FOR LOOP*************`)
+
+
+for (let i = 0; i <= resumeLength; i++) {
+        console.log(
+                ` 
+                  Name: ${resume.name}
+                  City: ${resume.city}
+                  Email: ${resume.contact.emailAddress}
+                  Contact No.: ${resume.contact.contactNo}
+
+                  Experience:
+
+                  ${resume.experience.firstCompany.companyName}
+                      Position: ${resume.experience.firstCompany.position}
+                      City: ${resume.experience.firstCompany.location}
+                      Duration: ${resume.experience.firstCompany.joiningDate} to ${resume.experience.firstCompany.lastWorkingDate}
+                      Desc: ${resume.experience.firstCompany.description}
+
+                  ${resume.experience.secondCompany.companyName}
+                      Position: ${resume.experience.secondCompany.position}
+                      City: ${resume.experience.secondCompany.location}
+                      Duration: ${resume.experience.secondCompany.joiningDate} to ${resume.experience.firstCompany.lastWorkingDate}
+                      Desc: ${resume.experience.secondCompany.description}
+
+                  Education:
+
+                   PostGraduation 
+                      University: ${resume.education.postGraduate.universityName}
+                      Subject: ${resume.education.postGraduate.subject}
+                      Year: ${resume.education.postGraduate.passYear}
+                   Graduation
+                      University: ${resume.education.underGraduate.universityName}
+                      Subject: ${resume.education.underGraduate.subject}
+                      Year: ${resume.education.underGraduate.passYear}
+                  
+                   Languages
+                      ${resume.language.hindi.name}
+                      ${resume.language.english.name}
+
+                   Skills
+                      IT Skill: ${resume.skills.ITSkill}
+                      Soft Skill: ${resume.skills.softSkill}
+
+                  `
+        );
+}
+
+
+// Using FOR IN LOOP FUNCTION
+console.log(`*************Using FOR IN LOOP*************`)
+// for (const key in object) {
+//         if (Object.hasOwnProperty.call(object, key)) {
+//                 const element = object[key];
+
+//         }
+// }
+
+for (let el in resume2) {
+        console.log(el, resume2[el]);
+};
+
+
+// Using FOR OF LOOP FUNCTION
+console.log(`*************Using FOR OF LOOP*************`)
+// for (const iterator of object) {
+
+// }
+for (const el of Object.keys(resume2)) {
+        console.log(el + ":" + resume2[el])
+};
+
+
+
+
+// Using FOR EACH LOOP FUNCTION
+console.log(`*************Using FOR EACH LOOP*************`)
+// array.forEach(element => {
+
+// });
+Object.keys(resume2).forEach(key => {
+        console.log(key, resume2[key]);
+});
